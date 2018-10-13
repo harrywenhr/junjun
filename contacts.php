aaa219e7a38160dd6b1cc9b44c701ad47664c559
HAORAN WEN <hwen@apple.com>
2018-10-13 01:38:52 -0700

    Revert "added contacts"
    This reverts commit fbfbe283825eb06a1659c54c7f23015c657c76e7.

--- contacts.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myContacts = file_get_contents("contacts.txt");
-$arrayOfContents = explode(PHP_EOL, $myContacts);
-foreach ($arrayOfContents as $contact) {
-    echo $contact;
-    echo "<br>";
-}
-fclose($myfile);
-?>
