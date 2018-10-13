1a8ca57f730af7a114bd17b4231530e6b53e3f9d
HAORAN WEN <hwen@apple.com>
2018-10-12 23:43:08 -0700

    initial commit

---
+++ fileRead.php
@@ -0,0 +1,11 @@
+<?php
+$myfileContents = fopen("test.txt", "r") or die("Unable to open file!");
+$myfileContents = file_get_contents("test.txt");
+//echo $myfileContents;
+$arrayOfContents = explode(',', $myfileContents);
+foreach ($arrayOfContents as $word) {
+    echo $word;
+    echo "\r\n";
+}
+fclose($myfile);
+?>
