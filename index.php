73e415743e1cb2ad6de2e0b438395026bdf00948
yijun zhang <zhangyijun0518@gmail.com>
2018-10-13 01:39:14 -0700

    changed all titles and contents, also added pics

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
 
 <body>
 	<!-- navigation -->
-	<nav class="pages-nav">
-		<div class="pages-nav__item"><a class="link link--page" href="#page-home">Home</a></div>
-		<div class="pages-nav__item"><a class="link link--page" href="#page-docu">Documentation</a></div>
-		<div class="pages-nav__item"><a class="link link--page" href="#page-manuals">Manuals</a></div>
-		<div class="pages-nav__item"><a class="link link--page" href="#page-software">Software</a></div>
-		<div class="pages-nav__item"><a class="link link--page" href="#page-custom">Customization &amp; Settings</a></div>
+	<nav class="pages-nav">		
+		    <div class="pages-nav__item"><a class="link link--page" href="#page-home">Home</a></div>
+<!-- 		<div class="pages-nav__item"><a class="link link--page" href="#page-docu">Customization &amp; Settings</a></div> -->
+		<div class="pages-nav__item"><a class="link link--page" href="#page-manuals">About</a></div>
+		<div class="pages-nav__item"><a class="link link--page" href="#page-software">News</a></div>
+		<div class="pages-nav__item"><a class="link link--page" href="#page-custom">Service</a></div>
 		<div class="pages-nav__item"><a class="link link--page" href="#page-training">Training</a></div>
-		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-buy">Where to buy</a></div>
-		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-blog">Blog &amp; News</a></div>
-		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-contact">Contact</a></div>
-		<div class="pages-nav__item pages-nav__item--social">
-			<a class="link link--social link--faded" href="#"><i class="fa fa-twitter"></i><span class="text-hidden">Twitter</span></a>
-			<a class="link link--social link--faded" href="#"><i class="fa fa-linkedin"></i><span class="text-hidden">LinkedIn</span></a>
-			<a class="link link--social link--faded" href="#"><i class="fa fa-facebook"></i><span class="text-hidden">Facebook</span></a>
-			<a class="link link--social link--faded" href="#"><i class="fa fa-youtube-play"></i><span class="text-hidden">YouTube</span></a>
+<!-- 		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-buy">Where to buy</a></div> -->
+<!-- 		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-blog">Blog &amp; News</a></div> -->
+<!-- 		<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-contact">Contact</a></div> -->
+<!-- 		<div class="pages-nav__item pages-nav__item--social"> -->
+<!-- 			<a class="link link--social link--faded" href="#"><i class="fa fa-twitter"></i><span class="text-hidden">Twitter</span></a> -->
+<!-- 			<a class="link link--social link--faded" href="#"><i class="fa fa-linkedin"></i><span class="text-hidden">LinkedIn</span></a> -->
+<!-- 			<a class="link link--social link--faded" href="#"><i class="fa fa-facebook"></i><span class="text-hidden">Facebook</span></a> -->
+<!-- 			<a class="link link--social link--faded" href="#"><i class="fa fa-youtube-play"></i><span class="text-hidden">YouTube</span></a> -->
 		</div>
 	</nav>
 	<!-- /navigation-->
@@ -42,21 +42,21 @@
 		<div class="page" id="page-home">
 			<!-- Blueprint header -->
 			<header class="bp-header cf">
-				<span class="bp-header__present">Blueprint <span class="bp-tooltip bp-icon bp-icon--about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
-				<h1 class="bp-header__title">Page Stack Navigation111</h1>
+				<span class="bp-header__present">Home <span class="bp-tooltip bp-icon bp-icon--about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
+				<h1 class="bp-header__title">Smart Company</h1>
 				<?php
-                        echo "This is PHP code";
-                        include 'fileRead.php';
-                        ?>
-				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
+//                         echo "This is PHP code";
+//                         include 'fileRead.php';
+//                         ?>
+<!-- 				<p class="bp-header__desc">Welcom! <a href="https://dribbble.com/shots/2286042-Stacked-navigation"></a></p> -->
 				<nav class="bp-nav">
-					<a class="bp-nav__item bp-icon bp-icon--prev" href="http://tympanus.net/Blueprints/ZoomSlider/" data-info="previous Blueprint"><span>Previous Blueprint</span></a>
+					<a class="bp-nav__item bp-icon bp-icon--prev" href="http://tympanus.net/Blueprints/ZoomSlider/" data-info="previous page"><span>Previous Blueprint</span></a>
 					<!--a class="bp-nav__item bp-icon bp-icon--next" href="" data-info="next Blueprint"><span>Next Blueprint</span></a-->
-					<a class="bp-nav__item bp-icon bp-icon--drop" href="http://tympanus.net/codrops/?p=25311" data-info="back to the Codrops article"><span>back to the Codrops article</span></a>
-					<a class="bp-nav__item bp-icon bp-icon--archive" href="http://tympanus.net/codrops/category/blueprints/" data-info="Blueprints archive"><span>Go to the archive</span></a>
+					<a class="bp-nav__item bp-icon bp-icon--drop" href="http://tympanus.net/codrops/?p=25311" data-info="back to the news"><span>back to the Codrops article</span></a>
+					<a class="bp-nav__item bp-icon bp-icon--archive" href="http://tympanus.net/codrops/category/blueprints/" data-info="email us"><span>Go to the archive</span></a>
 				</nav>
 			</header>
-			<img class="poster" src="images/1.jpg" alt="img01" />
+			<img class="poster" src="images/1.4.jpg" alt="img01" />
 		</div>
 		<!-- /page -->
 		<div class="page" id="page-docu">
@@ -67,34 +67,35 @@
 					"We cannot have peace among men whose hearts find delight in killing any living creature." &mdash; Rachel Carson
 				</p>
 			</header>
-			<img class="poster" src="images/6.jpg" alt="img06" />
+			<img class="poster" src="images/1.1.jpg" alt="img06" />
 		</div>
 		<div class="page" id="page-manuals">
 			<header class="bp-header cf">
-				<h1 class="bp-header__title">Manuals</h1>
-				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
+				<h1 class="bp-header__title">THE JOURNEY WITH CHILDREN BEGINS Here</h1>
+<!-- 				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p> -->
 				<p class="info">
-					"When you adopt a vegan diet we make a connection, you don't go back, it is not a diet, it is a lifestyle." &mdash; Freelee Frugivore
+					Smart Company provides service about early childhood education for preschool children in 0-6 years old.
+					This website will provide up-to-date knowledge and information in this field.
 				</p>
 			</header>
-			<img class="poster" src="images/2.jpg" alt="img02" />
+			<img class="poster" src="images/10.jpg" alt="img02" />
 		</div>
 		<div class="page" id="page-software">
 			<header class="bp-header cf">
-				<h1 class="bp-header__title">Software &amp; Downloads</h1>
-				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
+				<h1 class="bp-header__title">News</h1>
+				<p class="bp-header__desc">Latest information about early childhood education in Bay Area! <a href="https://dribbble.com/shots/2286042-Stacked-navigation"> </a></p>
 				<p class="info">
-					"I decided to pick the diet that I thought would maximize my chances of long-term survival." &mdash; Al Gore
+					Children are born angels, adult education into “human” them. &mdash; Al Gore
 				</p>
 			</header>
-			<img class="poster" src="images/3.jpg" alt="img03" />
+			<img class="poster" src="images/9.jpg" alt="img03" />
 		</div>
 		<div class="page" id="page-custom">
 			<header class="bp-header cf">
-				<h1 class="bp-header__title">Customization &amp; Settings</h1>
-				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
+				<h1 class="bp-header__title">Service</h1>
+<!-- 				<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p> -->
 				<p class="info">
-					"You have to make a conscious decision to change for your own well-being, that of your family and your country." &mdash;Bill Clinton
+					The services menu and online appointment system is coming soon!
 				</p>
 			</header>
 			<img class="poster" src="images/4.jpg" alt="img04" />
